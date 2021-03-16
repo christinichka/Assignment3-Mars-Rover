7a792036922f5c792d6879f3593e73eed7485863
Christina Varghese <46170618+christinichka@users.noreply.github.com>
2021-03-16 11:07:47 -0500

    Update rover.js
    Tests 1-11 and 13 pass. Still working on 12. 
    Next steps after test 12 passes is to comment back in the student grader spec and see if it passes the grading tests.

--- rover.js
+++ rover.js
@@ -18,33 +18,30 @@ class Rover {
       if (message.commands[i].commandType === 'MOVE') {
         
         if (this.mode === 'NORMAL') { 
-        this.mode = message.commands[i].value;
         this.position = message.commands[i].value;
-        response.results.push({completed: true});
+        response.results.push({completed: 'true'});
         
         } else if(this.mode === 'LOW_POWER') {
-        response.results.push({completed: false});
+        this.mode = 'LOW_POWER';
+        response.results.push({completed: 'false'});
 
       } else if (message.commands[i].commandType === 'MODE_CHANGE') {
-        if (this.mode === 'NORMAL') {
-          this.mode = 'NORMAL'
-        } else {
-          this.mode = 'LOW_POWER';
-          response.results.push({completed: true});
+          response.results.push({completed: 'true'});
+          this.mode = message.commands[i].value;
         }
             
       } else if (message.commands[i].commandType === 'STATUS_CHECK') {
-        response.results.push({completed: true, roverStatus: {mode: this.mode, generatorWatts: this.generatorWatts, position: this.position}});
+        response.results.push({completed: 'true', roverStatus: {mode: this.mode, generatorWatts: this.generatorWatts, position: this.position}});
       
       } else {
-        response.results.push({completed: false});
+        response.results.push({completed: 'false'});
       }
     }; 
     return response;   
-     
+    
   }
   
-}
-}
+};
+
 
 module.exports = Rover;
