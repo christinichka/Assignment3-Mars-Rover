2087ace3d936dc65746afb77850747ba7bc825ff
Christina Varghese <46170618+christinichka@users.noreply.github.com>
2021-03-15 21:28:00 -0500

    Update rover.js
    Added some conditionals to work on functionality of the rover, Curiosity.

--- rover.js
+++ rover.js
@@ -9,14 +9,42 @@ class Rover {
     message is an object
     results is an array of results that corresponds to one Command in message.commands*/
   receiveMessage(message) {
-    let results = [];
     let response = {
       message: message.name,
-    }
-    return response;
+      results: []
+    };
+    
+    for (let i = 0; i < (message.commands).length; i++) {
+      if (message.commands[i].commandType === 'MOVE') {
+        
+        if (this.mode === 'NORMAL') { 
+        this.mode = message.commands[i].value;
+        this.position = message.commands[i].value;
+        response.results.push({completed: true});
+        
+        } else if(this.mode === 'LOW_POWER') {
+        response.results.push({completed: false});
+
+      } else if (message.commands[i].commandType === 'MODE_CHANGE') {
+        if (this.mode === 'NORMAL') {
+          this.mode = 'NORMAL'
+        } else {
+          this.mode = 'LOW_POWER';
+          response.results.push({completed: true});
+        }
+            
+      } else if (message.commands[i].commandType === 'STATUS_CHECK') {
+        response.results.push({completed: true, roverStatus: {mode: this.mode, generatorWatts: this.generatorWatts, position: this.position}});
+      
+      } else {
+        response.results.push({completed: false});
+      }
+    }; 
+    return response;   
      
   }
   
+}
 }
 
 module.exports = Rover;
