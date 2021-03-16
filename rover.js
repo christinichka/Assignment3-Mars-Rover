cc1d7f0115e4491103a00e16dc08954c42c5e004
Christina Varghese <46170618+christinichka@users.noreply.github.com>
2021-03-16 16:34:15 -0500

    Update rover.js
    Tests 1-13 passed. Student grading spec passed except 1 test.

--- rover.js
+++ rover.js
@@ -18,19 +18,19 @@ class Rover {
       if (message.commands[i].commandType === 'MOVE') {
         
         if (this.mode === 'LOW_POWER') {
-        this.mode = 'LOW_POWER';
-        this.position = this.position;
-        response.results.push({completed: 'false'});
+        // this.mode = 'LOW_POWER';
+          this.position = this.position;
+          response.results.push({completed: 'false'});
         
-        } else if (this.mode === 'NORMAL') { 
-        this.position = message.commands[i].value;
-        response.results.push({completed: 'true'});       
-
+        } else {
+          this.mode = 'NORMAL';
+          this.position = message.commands[i].value;
+          response.results.push({completed: 'true'});       
+        }; 
       } else if (message.commands[i].commandType === 'MODE_CHANGE') {
         response.results.push({completed: 'true'});
         this.mode = message.commands[i].value;
-        }
-            
+      
       } else if (message.commands[i].commandType === 'STATUS_CHECK') {
         response.results.push({completed: 'true', roverStatus: {mode: this.mode, generatorWatts: this.generatorWatts, position: this.position}});
       
